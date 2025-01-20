53be628bb46f8c0d915f5e28c8d3ff61e3344ee1
Bill-MsDosUK <bill@msdos.uk>
2025-01-19 23:31:07 -0800

    Update PGVectorDatabase.cs

--- Oqtane.Database.PostgreSQL/PGVectorDatabase.cs
+++ Oqtane.Database.PostgreSQL/PGVectorDatabase.cs
@@ -6,73 +6,79 @@ using Microsoft.EntityFrameworkCore.Migrations.Operations;
 using Microsoft.EntityFrameworkCore.Migrations.Operations.Builders;
 using Npgsql;
 using Oqtane.Databases;
+using Pgvector;
 
 namespace Oqtane.Database.PGVector
 {
-    public class PGVectorDatabase : DatabaseBase
+   public class PGVectorDatabase : DatabaseBase
+{
+    private static string _friendlyName => "PGVector";
+    private static string _name => "PGVector";
+
+    static PGVectorDatabase()
     {
-        private static string _friendlyName => "PGVector";
-        private static string _name => "PGVector";
+        Initialize(typeof(PGVectorDatabase));
+    }
 
-        static PGVectorDatabase()
+    public PGVectorDatabase() : base(_name, _friendlyName) { }
+
+    public override string Provider => "Npgsql.EntityFrameworkCore.PostgreSQL";
+
+    public override OperationBuilder<AddColumnOperation> AddAutoIncrementColumn(ColumnsBuilder table, string name)
+    {
+        return table.Column<int>(name: name, nullable: false).Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn);
+    }
+
+    public override int ExecuteNonQuery(string connectionString, string query)
+    {
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        dataSourceBuilder.UseVector();
+        using var dataSource = dataSourceBuilder.Build();
+        using var conn = dataSource.OpenConnection();
+        var cmd = conn.CreateCommand();
+        using (conn)
         {
-            Initialize(typeof(PGVectorDatabase));
-        }
-
-        public PGVectorDatabase() : base(_name, _friendlyName) { }
-
-        public override string Provider => "Npgsql.EntityFrameworkCore.PostgreSQL";
-
-        public override OperationBuilder<AddColumnOperation> AddAutoIncrementColumn(ColumnsBuilder table, string name)
-        {
-            return table.Column<int>(name: name, nullable: false).Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityAlwaysColumn);
-        }
-
-        public override int ExecuteNonQuery(string connectionString, string query)
-        {
-            var conn = new NpgsqlConnection(connectionString);
-            var cmd = conn.CreateCommand();
-            using (conn)
-            {
-                PrepareCommand(conn, cmd, query);
-                var val = -1;
-                try
-                {
-                    val = cmd.ExecuteNonQuery();
-                }
-                catch
-                {
-                    // an error occurred executing the query
-                }
-                return val;
-            }
-        }
-
-        public override IDataReader ExecuteReader(string connectionString, string query)
-        {
-            var conn = new NpgsqlConnection(connectionString);
-            var cmd = conn.CreateCommand();
             PrepareCommand(conn, cmd, query);
-            var dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
-            return dr;
-        }
-
-        public override DbContextOptionsBuilder UseDatabase(DbContextOptionsBuilder optionsBuilder, string connectionString)
-        {
-            return optionsBuilder.UseNpgsql(connectionString)
-                .UseSnakeCaseNamingConvention()
-                .ReplaceService<IHistoryRepository, OqtaneHistoryRepository>();
-        }
-
-        private void PrepareCommand(NpgsqlConnection conn, NpgsqlCommand cmd, string query)
-        {
-            if (conn.State != ConnectionState.Open)
+            var val = -1;
+            try
             {
-                conn.Open();
+                val = cmd.ExecuteNonQuery();
             }
-            cmd.Connection = conn;
-            cmd.CommandText = query;
-            cmd.CommandType = CommandType.Text;
+            catch
+            {
+                // an error occurred executing the query
+            }
+            return val;
         }
+    }
+
+    public override IDataReader ExecuteReader(string connectionString, string query)
+    {
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(connectionString);
+        dataSourceBuilder.UseVector();
+        using var dataSource = dataSourceBuilder.Build();
+        using var conn = dataSource.OpenConnection();
+        var cmd = conn.CreateCommand();
+        PrepareCommand(conn, cmd, query);
+        var dr = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+        return dr;
+    }
+
+    public override DbContextOptionsBuilder UseDatabase(DbContextOptionsBuilder optionsBuilder, string connectionString)
+    {
+        return optionsBuilder.UseNpgsql(connectionString)
+            .UseSnakeCaseNamingConvention()
+            .ReplaceService<IHistoryRepository, OqtaneHistoryRepository>();
+    }
+
+    private void PrepareCommand(NpgsqlConnection conn, NpgsqlCommand cmd, string query)
+    {
+        if (conn.State != ConnectionState.Open)
+        {
+            conn.Open();
+        }
+        cmd.Connection = conn;
+        cmd.CommandText = query;
+        cmd.CommandType = CommandType.Text;
     }
 }
